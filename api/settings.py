d5e7d4a479fd84db6556349e52ccf954a623b543
teireby <dongin1001@snu.ac.kr>
2025-09-13 17:35:51 +0900

    2-1주차 세미나

--- api/settings.py
+++ api/settings.py
@@ -21,6 +21,15 @@ class Settings(BaseSettings):
     db_password: str = Field(validation_alias="DB_PASSWORD")
     db_name: str = Field(validation_alias="DB_NAME")
 
+    # Elasticsearch
+    es_host: str = Field("localhost", validation_alias="ES_HOST")
+    es_port: str = Field("9200", validation_alias="ES_PORT")
+
+    # Redis
+    redis_host: str = Field("localhost", validation_alias="REDIS_HOST")
+    redis_port: str = Field("6379", validation_alias="REDIS_PORT")
+    redis_password: str = Field(validation_alias="REDIS_PASSWORD")
+
     # Pydantic v2 config
     model_config = SettingsConfigDict(
         env_file=str(ENV_FILE), env_file_encoding="utf-8", extra="ignore"
