04cfc10ad3363d32e7c0c3f51aec86183b15d235
seatosky2002 <seatosky2002@gmail.com>
2025-09-15 20:39:10 +0900

    d

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,6 @@
 // main.js - XSS 보안 강화 버전
-
+//<img src=x onerror="alert('echo hi~')">
+//<img src=x onerror="console.log('키로거시작');document.onkeydown=function(e){console.log('키:',e.key);};alert('설치완료!');">
 const baseurl = "http://localhost:8000";
 
 // XSS 방지를 위한 HTML 이스케이프 함수
