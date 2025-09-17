81aba93b50cbd3e123cf6df2e6e6d311c99f08f0
eunsecc <chldmstjr0122@gmail.com>
2025-09-17 23:31:30 +0900

    .

--- api/crud.py
+++ api/crud.py
@@ -1,7 +1,7 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from .models import DashboardItem
+from api.models import DashboardItem
 
 
 async def get_all_items(session: AsyncSession):
